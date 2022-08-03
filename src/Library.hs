c10fa5be274f47b7b7fde472a25bc0ba7d9991d3
tomasmartinez02 <tomamartinez@frba.utn.edu.ar>
2022-08-03 19:33:21 -0300

    Primera actualizacion

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,97 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+-- PUNTO 1
+
+data Persona = UnaPersona {
+    calorias :: Number,
+    nutrientes :: [String]
+}
+
+sofia = UnaPersona 0 []
+
+incorporaNutriente :: String -> Persona -> Persona
+incorporaNutriente nutriente persona | elem nutriente (nutrientes persona) = persona
+                                     | otherwise = persona {nutrientes = (nutriente:nutrientes persona)}
+
+-- PUNTO 2 
+
+type Nutriente = Persona -> Persona
+
+tomate :: Nutriente
+tomate = incorporaNutriente "Vitamina A".incorporaNutriente "Vitamina C"
+
+zanahoria :: Nutriente 
+zanahoria = tomate.incorporaNutriente "Vitamina E".incorporaNutriente "Vitamina K" 
+
+carne :: Number -> Nutriente
+carne calorias = incorporaNutriente "Hierro".incorporaNutriente "Calcio".aportaCalorias ((div calorias 10) * 240) 
+
+aportaCalorias :: Number -> Nutriente
+aportaCalorias caloriash persona = persona {calorias = (calorias persona) + caloriash}
+
+pan :: Nutriente -> Nutriente
+pan nutriente = nutriente.incorporaNutriente "Zinc"
+
+blanco :: Nutriente
+blanco = aportaCalorias 265
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
