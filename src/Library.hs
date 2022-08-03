be457b7ccee2d3066217e7b34b0b9b949100e831
tomasmartinez02 <tomamartinez@frba.utn.edu.ar>
2022-08-03 20:30:20 -0300

    Segunda Actualizacion

--- src/Library.hs
+++ src/Library.hs
@@ -9,7 +9,7 @@ doble numero = numero + numero
 data Persona = UnaPersona {
     calorias :: Number,
     nutrientes :: [String]
-}
+}deriving(Show,Eq)
 
 sofia = UnaPersona 0 []
 
@@ -19,35 +19,85 @@ incorporaNutriente nutriente persona | elem nutriente (nutrientes persona) = per
 
 -- PUNTO 2 
 
-type Nutriente = Persona -> Persona
+type Comida = Persona -> Persona
 
-tomate :: Nutriente
+tomate :: Comida
 tomate = incorporaNutriente "Vitamina A".incorporaNutriente "Vitamina C"
 
-zanahoria :: Nutriente 
+zanahoria :: Comida 
 zanahoria = tomate.incorporaNutriente "Vitamina E".incorporaNutriente "Vitamina K" 
 
-carne :: Number -> Nutriente
-carne calorias = incorporaNutriente "Hierro".incorporaNutriente "Calcio".aportaCalorias ((div calorias 10) * 240) 
+carne :: Number -> Comida
+carne gramos = incorporaNutriente "Hierro".incorporaNutriente "Calcio".aportaCalorias ((div gramos 10) * 240) 
 
-aportaCalorias :: Number -> Nutriente
+aportaCalorias :: Number -> Comida
 aportaCalorias caloriash persona = persona {calorias = (calorias persona) + caloriash}
 
-pan :: Nutriente -> Nutriente
-pan nutriente = nutriente.incorporaNutriente "Zinc"
+pan :: Comida -> Comida
+pan tipo = tipo.incorporaNutriente "Zinc"
 
-blanco :: Nutriente
+blanco :: Comida
 blanco = aportaCalorias 265
 
+integral :: Comida
+integral = aportaCalorias 200.incorporaNutriente "Fibras"
+
+dePapa :: Comida 
+dePapa persona | estaPipona persona = aportaCalorias 100 persona
+               | otherwise = aportaCalorias 500 persona
+
+estaPipona :: Persona -> Bool
+estaPipona persona = calorias persona > 2000
+
+hamburguesaCheta :: Comida
+hamburguesaCheta = pan dePapa.carne 180.tomate.pan dePapa
+
+-- PUNTO 3
+
+type Menu = [Comida]
+
+comeMenu :: Menu -> Persona -> Persona
+comeMenu menu persona = foldl come persona menu
+
+come :: Persona -> Comida -> Persona
+come persona comida = comida persona
+
+menu1 :: Menu
+menu1 = [pan integral, zanahoria, hamburguesaCheta]
+
+-- PUNTO 4
+
+data Evento = UnEvento {
+    nombre :: String,
+    menu :: Menu,
+    invitados :: Invitados
+}
+
+type Invitados = [Persona]
+
+satisfecho :: Persona -> Bool
+--satisfecho persona = estaPipona persona || length(nutrientes persona) > 5
+satisfecho persona = estaPipona persona || ((>5).length.nutrientes) persona
+
+altaFiesta :: Evento -> Bool
+altaFiesta evento = all satisfecho (map (comeMenu (menu evento)) (invitados evento))
 
 
+-- PUNTO 5
 
+type Eventos = [Evento]
 
+valeLaPena :: Persona -> Evento -> Evento
+valeLaPena persona evento | altaFiesta evento = evento {invitados = (persona:invitados evento)}
+                        | otherwise = evento
 
+seInvita :: Persona -> Eventos -> Eventos
+seInvita persona eventos = map (valeLaPena persona) eventos  
 
-
-
-
+-- PUNTO 6
+-- No se podria determinar si un evento infinito es alta fiesta ya que utilizamos la funcion all, que busca analizar si todos los 
+-- invitados estan satisfechos. Es por esto que infinitamente se quedara analizando si todos los invitados estan satisfechos y 
+-- nunca podra determinar si es alta fiesta o no.
 
 
 
